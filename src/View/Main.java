38db999a1df8be5b09de392c9b60e800acfad3d2
Gabriel <gabrielmora2001@hotmail.com>
2021-05-13 08:26:22 -0500

    fondo

--- src/View/Main.java
+++ src/View/Main.java
@@ -4,7 +4,7 @@ import processing.core.PApplet;
 public class Main extends PApplet{
 
 	public static void main(String[] args) {
-		PApplet.main("Main");
+		PApplet.main(Main.class.getName());
 		
 	}
 	
